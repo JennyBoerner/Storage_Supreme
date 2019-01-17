6b3ec4295c93c7a2adfa8c3b682d171bbf9709aa
JoeDotPHP <Joe.Obrycki@gmail.com>
2019-01-16 18:04:41 -0600

    Updated version, another following shortly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,9 @@
 var db = require("../models");
 
+var express = require('express');
+
 module.exports = function(app) {
-  // Get all examples
+  // Grab all examples
   app.get("/api/examples", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
